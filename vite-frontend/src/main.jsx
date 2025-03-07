99743b4f0f05d36680c04a1177e09ea5d64075ed
Aman Arora <amanarora99005@gmail.com>
2025-03-07 11:02:54 +0530

    Updates

--- vite-frontend/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import { createRoot } from 'react-dom/client'
-import AppRoutes from './Routes/AppRoutes';
-import { StrictMode } from 'react';
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <AppRoutes />
-  </StrictMode>
-)
